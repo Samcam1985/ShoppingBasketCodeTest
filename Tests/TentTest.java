54a0a32a8e67d3db13a778f4aeb51b4337b8957f
Samantha Campbell <samantha.l.campbell1985@gmail.com>
2017-07-23 22:30:27 +0100

    Tent Tests refactored

--- Tests/TentTest.java
+++ Tests/TentTest.java
@@ -12,30 +12,6 @@ public class TentTest {
     tent = new Tent("8 Man Tent", 250.00, "Suitable for Ages 18+", "Regatta", false, 8);
   }
   
-  @Test
-  public void hasName() {
-    assertEquals("8 Man Tent", tent.getName());
-  }
-
-  @Test
-  public void hasPrice() {
-    assertEquals(250.00, tent.getPrice(), 0);
-  }
-
-  @Test 
-  public void hasDescription() {
-    assertEquals("Suitable for Ages 18+", tent.getDescription());
-  }
-
- @Test
- public void hasManufacturer() {
-  assertEquals("Regatta", tent.getManufacturer());
- }
-
- @Test
- public void hasBuyOneGetOneFree() {
-  assertEquals(false, tent.getHasBuyOneGetOneFree());
- }
 
   @Test
   public void hasSize() {
