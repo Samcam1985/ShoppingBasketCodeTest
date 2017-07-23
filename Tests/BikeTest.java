54a870c45e8c2c17323e0559167b6ee4c80ba989
Samantha Campbell <samantha.l.campbell1985@gmail.com>
2017-07-23 22:26:50 +0100

    Bike Tests refactored

--- Tests/BikeTest.java
+++ Tests/BikeTest.java
@@ -12,30 +12,6 @@ public class BikeTest {
     bike = new Bike("Bicycle", 99.99, "Suitable for Ages 12+", "Raleigh", false, "Mountain", 2);
   }
   
-  @Test
-  public void hasName() {
-    assertEquals("Bicycle", bike.getName());
-  }
-
-  @Test
-  public void hasPrice() {
-    assertEquals(99.99, bike.getPrice(), 0);
-  }
-
-  @Test 
-  public void hasDescription() {
-    assertEquals("Suitable for Ages 12+", bike.getDescription());
-  }
-
- @Test
- public void hasManufacturer() {
-  assertEquals("Raleigh", bike.getManufacturer());
- }
-
- @Test
- public void hasBuyOneGetOneFree() {
-  assertEquals(false, bike.getHasBuyOneGetOneFree());
- }
 
   @Test
   public void hasType() {
