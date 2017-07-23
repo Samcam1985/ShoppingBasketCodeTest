a91c3890a19abfb346842fe739afccf2d3a2b261
Samantha Campbell <samantha.l.campbell1985@gmail.com>
2017-07-23 22:29:55 +0100

    Racquet Tests refactored

--- Tests/RacquetTest.java
+++ Tests/RacquetTest.java
@@ -11,32 +11,7 @@ public class RacquetTest {
   public void before(){
     racquet = new Racquet("Power Racquet", 50.00, "Suitable for Ages 12+", "Adidas", true, "Tennis Racquet", 36);
   }
-  
-  @Test
-  public void hasName() {
-    assertEquals("Power Racquet", racquet.getName());
-  }
-
-  @Test
-  public void hasPrice() {
-    assertEquals(50.00, racquet.getPrice(), 0);
-  }
-
-  @Test 
-  public void hasDescription() {
-    assertEquals("Suitable for Ages 12+", racquet.getDescription());
-  }
-
- @Test
- public void hasManufacturer() {
-  assertEquals("Adidas", racquet.getManufacturer());
- }
-
- @Test
- public void hasBuyOneGetOneFree() {
-  assertEquals(true, racquet.getHasBuyOneGetOneFree());
- }
-
+ 
   @Test
   public void hasType() {
     assertEquals("Tennis Racquet", racquet.getType());
